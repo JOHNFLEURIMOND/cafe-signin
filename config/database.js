889e1876235a950c5d68e2625ef23276d076b348
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2018-03-19 16:53:24 -0400

    First commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+  'url': 'mongodb://godsplan:MiltonMass2018@ds039195.mlab.com:39195/to-do' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
